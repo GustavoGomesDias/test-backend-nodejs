1c2863d6d0ad4f0e3c46bff3fb95de440ef72750
GustavoGomesDias <treeelevengg@gmail.com>
2021-05-30 20:28:48 -0300

    feat: add category model

---
+++ src/models/Category.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const Category = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model('Category', Category);
