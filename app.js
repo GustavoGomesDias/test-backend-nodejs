5438092d96a431b3428fbe43e7ff6c02e5de0fa3
GustavoGomesDias <treeelevengg@gmail.com>
2021-05-30 19:45:38 -0300

    feat: add mongoose config and .env examples

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose.connect(`mongodb://${process.env.DATABASE_URL}:${process.envDATABASE_PORT}/${process.env.DATABASE}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.urlencoded({ extended: true }));
 
